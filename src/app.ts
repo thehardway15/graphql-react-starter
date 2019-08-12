0f45fd6881855bcda78492b45d1bca5ac61a0939
Damian Wiśniewski <d.wisniewski@uniso.pl>
2019-08-12 14:05:31 +0200

    Fix app listen.

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,10 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Start to work!');
 });
 
-app.listen(port, err => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`server is listening on ${port}`);
-});
+app.listen(port, () => console.log(`server is listening on ${port}`));
